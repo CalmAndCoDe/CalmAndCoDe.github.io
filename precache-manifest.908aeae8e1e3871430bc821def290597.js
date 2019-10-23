ab4be243946b606ce7e0bd8fcfe5710b34d7bf41
calmandcode <hitmanabsolution81@gmail.com>
2019-10-23 19:03:44 +0430

    push

---
+++ precache-manifest.908aeae8e1e3871430bc821def290597.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9448a7a338afeea69a7c47483a879e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e64c0c57a7a7a481598f2b050275575",
+    "url": "/img/projects/3.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "82374b67db3f3e48fcf5",
+    "url": "/js/chunk-vendors.591f7471.js"
+  },
+  {
+    "revision": "bec00959b77033ad9370",
+    "url": "/js/app.39f20fc1.js"
+  },
+  {
+    "revision": "651b22ea751fd5bb68ac6709201556fa",
+    "url": "/img/projects/2.png"
+  },
+  {
+    "revision": "b1f191ece15b05a81d5bd63d8d7d5bfb",
+    "url": "/img/projects/1.png"
+  },
+  {
+    "revision": "7b539cd6212db1bc108a0f475d91d84b",
+    "url": "/img/pic.7b539cd6.jpg"
+  },
+  {
+    "revision": "3e4cc632dbce29c18fc2e8ed1d43b08d",
+    "url": "/img/instagram.3e4cc632.svg"
+  },
+  {
+    "revision": "9f47a8b89b1b216822b03c840bf41825",
+    "url": "/img/img1.png"
+  },
+  {
+    "revision": "5f5a0c7d2a799923c33be42a02f0bb3a",
+    "url": "/img/img.png"
+  },
+  {
+    "revision": "cf4fe48d2138befc871f86f6633a63c4",
+    "url": "/img/github.cf4fe48d.svg"
+  },
+  {
+    "revision": "55e2411923429f5cbfd1450aa9dcbee3",
+    "url": "/img/bg.png"
+  },
+  {
+    "revision": "82374b67db3f3e48fcf5",
+    "url": "/css/chunk-vendors.f128a6ce.css"
+  },
+  {
+    "revision": "bec00959b77033ad9370",
+    "url": "/css/app.a9ddc655.css"
+  }
+];
